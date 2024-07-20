bfb17994b09ed4d4f6fe4a2bc611ca43ab236fa4
Xineraa <xinerayt@gmail.com>
2024-07-20 15:44:01 +0200

    Added a placeholder parallax background and added a parallax script. Also changed from tilemap to a custom ground for more flexibility

---
+++ PSGameJam15/Assets/Scripts/Parallax.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public Camera cam;
+    public Transform subject;
+
+    Vector2 startPosition;
+    float startZ;
+
+    Vector2 travel => (Vector2)cam.transform.position - startPosition;
+    float distFromSubject => transform.position.z - subject.position.z;
+    float clippingPlane => cam.transform.position.z + (distFromSubject > 0 ? cam.farClipPlane : cam.nearClipPlane);
+    float parallaxFactor => Mathf.Abs(distFromSubject) / clippingPlane;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        startZ = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newPos = transform.position = startPosition + travel * parallaxFactor;
+        transform.position = new Vector3(newPos.x, newPos.y, startZ);
+    }
+}
